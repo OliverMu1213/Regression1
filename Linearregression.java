50ac6172e44a88aed20de5a8bac440f08cc54bc9
OliverMu1213 <35769239+OliverMu1213@users.noreply.github.com>
2018-10-17 09:56:22 -0400

    Update Linearregression.java

--- Linearregression.java
+++ Linearregression.java
@@ -7,10 +7,10 @@ public class Linearregression {
 
     public static void main(String[] args) throws FileNotFoundException {
         String a = "";
-        Scanner Demarcus = new Scanner(new File("/Users/luyu_mu/Downloads/EgData.csv"));
-        scanner.useDelimiter(",");
-        while(scanner.hasNext()){
-            System.out.print(scanner.next()+"|");
+        Scanner user = new Scanner(new File("/Users/luyu_mu/Downloads/EgData.csv"));
+        user.useDelimiter(",");
+        while(user.hasNext()){
+            System.out.print(user.next()+"|");
         }
 
     }
